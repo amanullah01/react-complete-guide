67d42a7d406ce5a73039fc3b715e4d0f5a8349dd
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-03 22:56:03 +0600

    let const

---
+++ 02-javascript-refresher/let-const.js
@@ -0,0 +1,12 @@
+let myName = "Aman";
+console.log(myName);
+
+myName = "Khan";
+console.log(myName);
+
+const yourName = "Mr zoom";
+console.log(yourName);
+
+//not work.. because youName is const variable
+yourName = "Zoomla";
+console.log(yourName);
