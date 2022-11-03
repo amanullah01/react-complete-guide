1c81ff835bf8003c42df85d43f38f95d2e5b830a
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-03 23:00:22 +0600

    arrow function

---
+++ 02-javascript-refresher/arrow-function.js
@@ -0,0 +1,15 @@
+function myFunction(name) {
+  console.log(name);
+}
+myFunction("Aman");
+
+//arrow function
+const myNameFunc = (myName) => {
+  return myName;
+};
+
+console.log(myNameFunc("Amanullah Aman"));
+
+//example 2
+const multiply = (number) => number * 2;
+console.log(multiply(5));
