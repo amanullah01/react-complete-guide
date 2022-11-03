b203b14c01178aebfc780b45c2dd0d604b73fe09
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-03 23:58:37 +0600

    rest operator

--- 02-javascript-refresher/spread-rest.js
+++ 02-javascript-refresher/spread-rest.js
@@ -6,3 +6,7 @@ console.log(newNumbers);
 let person = { username: "aman" };
 let personWithAge = { ...person, age: 40 };
 console.log(personWithAge);
+
+//rest operator
+const sort = (...numbers) => numbers.sort((a, b) => a - b);
+console.log(sort(1, 3, 4, 2, 30, 24));
